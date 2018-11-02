3b4a66b06d5ed268e5ab7cd825d2334b154fcb2a
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-02 09:05:17 +0200

    Add files via upload

---
+++ bot24_1.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const other = new Discord.Client();
+const config = require('./config.json');
+
+
+other.on('ready', () => {
+  console.log(`Logged in as ${other.user.tag}!`);
+});
+
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+other.channels.get("507275523349676034").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},4000);
+});
+
+other.login(config.token24);
